2cfe1142e5b8483392236e197d01410f4307994b
Wito De Greef <witodegreef@Witos-MacBook-Pro-2.local>
2023-10-25 22:32:47 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,324 @@
+const rounds = [
+    // Opwarmers
+    {
+        name: "Oooooopwaaaaarmmmertttjjeeeeuuuu",
+        questions: [
+            // Algemeen
+            {
+                question: "Welke van deze is geen traditionele Halloweenversiering?",
+                answers: ["Ketel", "Kaars", "Heks", "Spin", "Krans", "Skelet", "Pompoen"],
+                correctAnswer: 4
+            },
+            {
+                question: "Wat is het record voor het aantal verlichte jack-o'-lanterns op display?",
+                answers: ["28 367", "29 433", "30 851", "31 225"],
+                correctAnswer: 2
+            },
+            {
+                question: "Welke kleur staat voor de oogst?",
+                answers: ["Geel", "Oranje", "Bruin", "Groen"],
+                correctAnswer: 1
+            },
+            {
+                question: "Welke kleur betekent de dood?",
+                answers: ["Grijs", "Wit", "Zwart", "Geel"],
+                correctAnswer: 2
+            },
+            {
+                question: "Wat is het populairste Halloween-kostuum in de VS volgens Google?",
+                answers: ["Een heks", "Peter Pan", "Pompoen", "Een clown"],
+                correctAnswer: 0
+            }
+        ]
+    },
+    // Geschiedenis
+    {
+        name: "Back To The Future (eeuuuhh nee past hé)",
+        questions: [
+            {
+                question: "Halloween werd gestart door welke groep mensen?",
+                answers: ["Vikingen", "Moren", "Kelten", "Romeinen"],
+                correctAnswer: 2
+            },
+            {
+                question: "Wat betekent het woord 'Halloween'?",
+                answers: ["Heiligenavond", "Enge nacht", "Herdenkingsdag", "Snoependag"],
+                correctAnswer: 0
+            },
+            {
+                question: "In 1000 na Christus paste welke religie Halloween aan om bij hun eigen gewoonten te passen?",
+                answers: ["Jodendom", "Christendom", "Islam", "Confucianisme"],
+                correctAnswer: 1
+            },
+            {
+                question: "In welk land is Halloween begonnen?",
+                answers: ["Brazilië", "India", "Duitsland", "Ierland"],
+                correctAnswer: 3
+            },
+            {
+                question: "Vóór pompoenen, welke wortelgroente sneden de Ieren en Schotten uit met Halloween?",
+                answers: ["Bloemkolen", "Meiraapjes", "Wortels", "Aardappelen"],
+                correctAnswer: 1
+            },
+            {
+                question: "Welke bekende goochelaar stierf op Halloween?",
+                answers: ["David Copperfield", "Criss Angel", "Harry Houdini", "David Blaine"],
+                correctAnswer: 2
+            },
+            {
+                question: "Wat is de originele naam van Halloween?",
+                answers: ["Mischief Night", "Ghouls and Goblins Eve", "Jack-O'-Lantern Festival", "All Hallows' Eve"],
+                correctAnswer: 3
+            },
+            {
+                question: "Waarom werd er initieel voor Halloween verkleed als spoken en geesten?",
+                answers: ["Om de geesten te aanbidden", "Als vermoming", "Als manier om dingen ter stelen", "Om de buren te pranken"],
+                correctAnswer: 1
+            },
+            {
+                question: "Welke cartoon is sinds zijn ontstaan in 1966 elke oktober afgespeeld?",
+                answers: ["It's the Great Pumpkin, Charlie Brown", "A Smurfy Halloween", "Mickey Mouse and the Ghouls", "Scooby-Doo to the Rescue"],
+                correctAnswer: 0
+            },
+            {
+                question: "Het zien van wat op Halloween brengt volgens sommige geluk?",
+                answers: ["Vleermuis", "Zwarte kat", "Spin", "Pompoen"],
+                correctAnswer: 2
+            }
+        ]
+    },
+    // Vanop Den Televies
+    {
+        name: "Vanop Den Televies",
+        questions: [
+            {
+                question: "Wie is Wednesday Addams in het Addams Family?",
+                answers: ["Dochter", "Moeder", "Vader", "Zoon"],
+                correctAnswer: 0
+            },
+            {
+                question: "In de klassieker uit 1966 'It's the Great Pumpkin, Charlie Brown', welk personage legt het verhaal van de Great Pumpkin uit?",
+                answers: ["Snoopy", "Sally", "Linus", "Schroeder"],
+                correctAnswer: 2
+            },
+            {
+                question: "Hoe heette de kreeft die uit de tank werd gehaald in Hocus Pocus?",
+                answers: ["Jimmy", "Falla", "Michael", "Angelo"],
+                correctAnswer: 3
+            },
+            {
+                question: "Wat is verboden in Hollywood op Halloween?",
+                answers: ["Pompoensoep", "Ballonnen", "Silly string", "Candy corn"],
+                correctAnswer: 2
+            },
+            {
+                question: "Welke kleur heeft Frankenstein in Hotel Transylvania?",
+                answers: ["groen", "grijs", "wit", "blauw"],
+                correctAnswer: 3
+            },
+            {
+                question: "De drie heksen in Hocus Pocus zijn Winnie, Mary en wie?",
+                answers: ["Sarah", "Hannah", "Jennie", "Daisy"],
+                correctAnswer: 0
+            },
+            {
+                question: "Welk dier begroeven Wednesday en Pugsley aan het begin van Adams Family Values?",
+                answers: ["een hond", "een varken", "een kat", "een kip"],
+                correctAnswer: 2
+            },
+            {
+                question: "In welke vorm is de strik van de burgemeester in The Nightmare Before Christmas?",
+                answers: ["een auto", "een spin", "een hoed", "een kat"],
+                correctAnswer: 1
+            },
+            {
+                question: "Hoeveel wezens trekken Jack's slee in The Nightmare Before Christmas, inclusief Zero?",
+                answers: ["3", "4", "5", "6"],
+                correctAnswer: 1
+            },
+            {
+                question: "Wat is NIET iets dat we zien in Monster House?",
+                answers: ["een driewieler", "een vlieger", "een hoed", "schoenen"],
+                correctAnswer: 3
+            }
+        ]
+    },
+    // Friends
+    {
+        name: "The One With The Halloween Party",
+        questions: [
+            {
+                question: "Als wat was Ross verkleed op Halloween?",
+                answers: ["Gordeldier", "Dinosaurus", "Spudnik", "Cowboy"],
+                correctAnswer: 2
+            },
+            {
+                question: "Wat denken Joey en Chandler dat Ross zijn kostum is?",
+                answers: ["Pannekoek", "Doody", "Mr. Patato Head", "Papieren zak"],
+                correctAnswer: 1
+            },
+            {
+                question: "Welk gezicht maakt Chandler tijdens het worstelen volgens Monica?",
+                answers: ["Bedrukt gezicht", "Boos gezicht", "Winnaars gezicht", "Seks gezicht"],
+                correctAnswer: 3
+            },
+            {
+                question: "Wat was het kostum van Eric?",
+                answers: ["Worstelaar", "Duivel", "Zonnestelsel", "Vampier"],
+                correctAnswer: 2
+            },
+            {
+                question: "Hoeveel jonger is Pheobe t.o.v. Ursula?",
+                answers: ["10 minuten", "10 seconden", "1 minuut", "1 uur"],
+                correctAnswer: 2
+            },
+            {
+                question: "Toen Eric in het Vredeskorps zat, waar zei hij dat hij huizen bouwde?",
+                answers: ["Haïti", "Congo", "Panama", "Uruguay"],
+                correctAnswer: 3
+            },
+            {
+                question: "Vanuit welk gebouw kwam Ursula in het begin van deze aflevering?",
+                answers: ["De bloemenwinkel", "De bank", "Het restaurant waarin ze werkt", "De kunstgallerij"],
+                correctAnswer: 0
+            },
+            {
+                question: "Wanneer Monica aan Joey vraagt, 'You read comic books, right?', hoe reageert hij?",
+                answers: ["A bit", "Exclusively", "Everyday", "You know me well"],
+                correctAnswer: 1
+            },
+            {
+                question: "Wie bracht snoep naar het feestje wanneer Rachel de hele eerste batch weg gaf?",
+                answers: ["Frank Jr.", "Ursula", "Gunther", "Judy Geller"],
+                correctAnswer: 2
+            },
+            {
+                question: "Wat vond Phoebe op het einde wanneer in Ursula haar handtas, om te bewijzen aan Eric dat ze loog?",
+                answers: ["Eén van Ursula's porno video's", "Haar geboortecertificaat", "Een aansteker", "Vloerplannen van de bank"],
+                correctAnswer: 3
+            },
+        ]
+    }
+];
+
+let currentRound = 0;
+let currentQuestion = 0;
+
+function loadRound() {
+    const roundName = document.getElementById('round-name');
+    const startRoundButton = document.getElementById('start-round-btn');
+    const questionText = document.getElementById('question');
+    const questionImage = document.getElementById('question-image');
+    const answersList = document.getElementById('answers');
+    const nextBtn = document.getElementById("next-btn");
+
+    const currentR = rounds[currentRound];
+    roundName.textContent = "Ronde " + (currentRound + 1) + ": " +  currentR.name;
+    startRoundButton.style.display = 'block';
+    questionText.style.display = 'none';
+    questionImage.style.display = 'none';
+    answersList.style.display = 'none';
+    nextBtn.style.display = "none";
+}
+
+function startRound() {
+    const roundName = document.getElementById('round-name');
+    const startRoundButton = document.getElementById('start-round-btn');
+    const questionText = document.getElementById('question');
+    const answersList = document.getElementById('answers');
+    const nextBtnWrapper = document.getElementById("next-btn-wrapper");
+
+    roundName.textContent = '';
+    startRoundButton.style.display = 'none';
+    questionText.style.display = 'block';
+    answersList.style.display = 'flex';
+    nextBtnWrapper.style.display = "block";
+
+    // Start de vragen voor de huidige ronde
+    loadQuestion();
+}
+
+function loadQuestion() {
+    // Get the html elements
+    const questionText = document.getElementById('question');
+    const questionImage = document.getElementById('question-image');
+    const answersList = document.getElementById('answers');
+
+    // Get current question
+    const round = rounds[currentRound];
+    const currentQ = round.questions[currentQuestion];
+    
+    // Fill in question
+    questionText.textContent = currentQ.question;
+
+    // Place image
+    if (!!currentQ.image) {
+        questionImage.src = currentQ.image;
+        questionImage.style.display = "block";
+    }
+    else {
+        questionImage.style.display = "none";
+    }
+
+    // Hide the next button again
+    document.getElementById("next-btn").style.display = "none";
+
+    // Fill in the answers
+    answersList.innerHTML = '';
+    currentQ.answers.forEach((answer, index) => {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.textContent = answer;
+        button.onclick = () => checkAnswer(index);
+        li.appendChild(button);
+        answersList.appendChild(li);
+    });
+}
+
+function checkAnswer(selectedIndex) {
+    const round = rounds[currentRound];
+    const question = round.questions[currentQuestion];
+    if (selectedIndex === question.correctAnswer) {
+        document.getElementById("answers").children[selectedIndex].style.backgroundColor = "green";
+        document.getElementById("next-btn").style.display = "block";
+    } else {
+        document.getElementById("answers").children[selectedIndex].style.backgroundColor = "red";
+    }
+}
+
+function nextQuestion() {
+    // Verwijder de antwoordmarkeringen voordat je naar de volgende vraag gaat
+    const answerButtons = document.getElementById("answers").getElementsByTagName("button");
+    Array.from(answerButtons).forEach(button => {
+        button.style.backgroundColor = "";
+    });
+
+    // Check if we can continue in round
+    const round = rounds[currentRound];
+    currentQuestion++;
+    if (currentQuestion < round.questions.length) {
+        loadQuestion();
+        return;
+    } 
+
+    // Check if we can start another round
+    currentRound++;
+    if (currentRound < rounds.length) {
+        loadRound();
+        return;
+    }
+
+    // All questions have been finished
+    document.getElementById("question").textContent = "Quiz voltooid!";
+    document.getElementById("question-image").src = "";
+    document.getElementById("answers").innerHTML = "";
+    document.getElementById("next-btn").style.display = "none";
+}
+
+function start() {
+    const startRoundButton = document.getElementById('start-round-btn');
+    startRoundButton.onclick = startRound;
+    startRoundButton.innerText = "Start Ronde"
+
+    loadRound();
+}
